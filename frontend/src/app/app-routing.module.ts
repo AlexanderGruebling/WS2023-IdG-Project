14c513fe3339d37566e50041966fdb62af205b11
Alexander Grübling <e12122371@student.tuwien.ac.at>
2023-11-26 11:38:35 +0100

    [TASK] Add FE capability to add medications; refs 3

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
+import {AddMedsComponent} from './components/add-meds/add-meds.component';
 import {AuthGuard} from './guards/auth.guard';
 import {RegistrationComponent} from './components/registration/registration.component';
 import {ProfileComponent} from './components/profile/profile.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'registration', component: RegistrationComponent},
   {path: 'profile', component: ProfileComponent},
+  {path: 'add-meds', component: AddMedsComponent},
 ];
 
 @NgModule({
---
+++ frontend/src/app/components/add-meds/add-meds.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {Medication} from '../../dtos/Medication';
+import {MedicationService} from '../../services/medication.service';
+import {ToastrService} from 'ngx-toastr';
+
+@Component({
+  selector: 'app-add-meds',
+  templateUrl: './add-meds.component.html',
+  styleUrls: ['./add-meds.component.scss']
+})
+export class AddMedsComponent implements OnInit {
+  numberOfMeds = 1;
+  meds: Set<Medication>;
+
+  constructor(
+    private service: MedicationService,
+    private notification: ToastrService,
+  ) { }
+
+  ngOnInit(): void {
+    console.log('add-meds loaded');
+    this.meds = new Set<Medication>();
+  }
+
+  updateMeds(med: Medication) {
+    this.meds.add(med);
+    console.log(this.meds);
+  }
+
+  addMeds() {
+    this.meds.forEach(med => this.service.create(med).subscribe({
+      next: data => {
+        console.log(`${med.name} successfully created`);
+      },
+      error: err => {
+        console.error('Error creating med', err);
+        this.notification.error(`Could not create Medication ${med.name}!` );
+      }
+    }
+    ));
+  }
+
+}
