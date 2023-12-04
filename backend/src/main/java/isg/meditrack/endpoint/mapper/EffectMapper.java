e1d4fe602d0288e611b49357fdf4fee9fbc453bc
Armin Kazda <e11909468@student.tuwien.ac.at>
2023-12-04 16:51:15 +0100

    [FEATURE] Added effect endpoint methods to get a specific one, and for an effect or a medication

--- backend/src/main/java/isg/meditrack/endpoint/mapper/EffectMapper.java
+++ backend/src/main/java/isg/meditrack/endpoint/mapper/EffectMapper.java
@@ -4,8 +4,11 @@ import isg.meditrack.endpoint.dto.EffectDto;
 import isg.meditrack.entity.Effect;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface EffectMapper {
     Effect effectDtoToEffect(EffectDto effectDto);
     EffectDto effectToEffectDto(Effect effect);
+    List<EffectDto> effectListToEffectDtoList(List<Effect> effects);
 }
--- backend/src/main/java/isg/meditrack/service/impl/EffectServiceImpl.java
+++ backend/src/main/java/isg/meditrack/service/impl/EffectServiceImpl.java
@@ -2,13 +2,17 @@ package isg.meditrack.service.impl;
 
 import isg.meditrack.entity.Effect;
 import isg.meditrack.entity.Entry;
+import isg.meditrack.entity.Medication;
+import isg.meditrack.exception.NotFoundException;
 import isg.meditrack.repository.EffectRepository;
 import isg.meditrack.service.EffectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.lang.invoke.MethodHandles;
+import java.util.List;
 
 @Service
 public class EffectServiceImpl implements EffectService {
@@ -27,5 +31,27 @@ public class EffectServiceImpl implements EffectService {
         newEffect.setEntry(entry);
         newEffect = effectRepository.save(newEffect);
         return newEffect;
+    }
+
+    @Override
+    public Effect getById(Long id) {
+        LOGGER.debug("Get medication {}", id);
+
+        return effectRepository.getReferenceById(id);
+    }
+
+    @Override
+    public List<Effect> getByMedication(Long medId) {
+        LOGGER.debug("Get effects by Medication");
+
+
+        return effectRepository.findAllByMedId(medId);
+    }
+    @Override
+    public List<Effect> getByEntry(Long entryId) {
+        LOGGER.debug("Get effects by Entry");
+
+
+        return effectRepository.findAllByEntryId(entryId);
     }
 }
