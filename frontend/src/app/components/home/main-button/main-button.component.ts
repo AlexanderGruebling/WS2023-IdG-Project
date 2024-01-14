8f75bf2c5e1722f340510425db71fcb339445828
Alexander Grübling <e12122371@student.tuwien.ac.at>
2024-01-14 23:03:44 +0100

    [TASK] Add Images and refresh home screen design

--- frontend/src/app/components/home/main-button/main-button.component.ts
+++ frontend/src/app/components/home/main-button/main-button.component.ts
@@ -9,11 +9,29 @@ export class MainButtonComponent implements OnInit {
   @Input() title = '';
   @Input() refersTo = '';
   @Input() buttonIcon = '';
-  public icon = '';
+  text = '';
+  asset = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    switch (this.buttonIcon) {
+      case 'calendar-plus':
+        this.text = 'Track your prescription intake';
+        this.asset = 'pills.jpg';
+        break;
+      case 'calendar-date':
+        this.text = 'View your daily intake';
+        this.asset = 'calendar.jpg';
+        break;
+      case 'bar-chart':
+        this.text = 'Gain new insights with charts';
+        this.asset = 'chart.jpg';
+        break;
+      case 'person-circle':
+        this.text = 'Manage your profile';
+        break;
+    }
   }
 
 }
