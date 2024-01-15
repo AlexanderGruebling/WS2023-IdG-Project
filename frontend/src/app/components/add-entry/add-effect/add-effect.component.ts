4df8331ea34441e3ace8f75ee7c50d0bdfbb9cfb
Alexander Grübling <e12122371@student.tuwien.ac.at>
2024-01-15 14:55:30 +0100

    [BUGFIX] Fix intensity always 0 bug

--- frontend/src/app/components/add-entry/add-effect/add-effect.component.ts
+++ frontend/src/app/components/add-entry/add-effect/add-effect.component.ts
@@ -11,13 +11,12 @@ export class AddEffectComponent implements OnInit {
   @Input() effectName = '';
   @Input() currentMed: Medication;
   @Output() effectEvent = new EventEmitter<Effect>();
-  range = 0;
   effect: Effect;
 
   constructor() {
   }
   ngOnInit() {
-    this.effect = new Effect(this.effectName, '', this.range,false, this.currentMed.medId);
+    this.effect = new Effect(this.effectName, '', 0,false, this.currentMed.medId);
   }
   addNewEffect() {
     this.effectEvent.emit(this.effect);
