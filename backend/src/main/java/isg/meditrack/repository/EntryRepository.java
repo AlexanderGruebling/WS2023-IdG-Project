45700ad1f93537df155dd0fae8b58b8d59b48080
Armin Kazda <e11909468@student.tuwien.ac.at>
2024-01-23 19:10:50 +0100

    [FIX] Entries are now sorted by date with newest first. Changed title design on main page

--- backend/src/main/java/isg/meditrack/repository/EntryRepository.java
+++ backend/src/main/java/isg/meditrack/repository/EntryRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository public interface EntryRepository extends JpaRepository<Entry, Long> {
-    @Query("SELECT e FROM Entry e WHERE e.user.id = ?1")
+    @Query("SELECT e FROM Entry e WHERE e.user.id = ?1 ORDER BY e.date DESC")
     List<Entry> findAllByUserId(Long userId);
 
     @Query("SELECT e FROM Entry e WHERE e.user.id = ?1 ORDER BY e.date DESC")
